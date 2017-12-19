af48c197bca67e112a70218fe6e4e89bca746bdf
Oleg <avdeevoleg91@gmail.com>
2017-12-19 22:14:54 +0200

    ver.1

--- 1_calculator/Program.cs
+++ 1_calculator/Program.cs
@@ -10,6 +10,43 @@ namespace _1_calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Select the arithmetical operation: \n" +
+                "1. Multiplication \n" +
+                "2. Divide \n" +
+                "3. Addition \n" +
+                "4. Sutraction \n" +
+                "5. Exponintation \n");
+
+            int operation = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the first operand:");
+
+            int firstOperand = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the first operand:");
+
+            int secondOperand = int.Parse(Console.ReadLine());
+
+            if (operation == 1)
+            {
+                Console.WriteLine("{0} * {1} = {2}", firstOperand, secondOperand, firstOperand * secondOperand);
+            }
+            else if (operation == 2)
+            {
+                Console.WriteLine("{0} / {1} = {2}", firstOperand, secondOperand, firstOperand / secondOperand);
+            }
+            else if (operation == 3)
+            {
+                Console.WriteLine("{0} + {1} = {2}", firstOperand, secondOperand, firstOperand + secondOperand);
+            }
+            else if (operation == 4)
+            {
+                Console.WriteLine("{0} _ {1} = {2}", firstOperand, secondOperand, firstOperand - secondOperand);
+            }
+            else if (operation == 5)
+            {
+                Console.WriteLine("{0} ^ {1} = {2}", firstOperand, secondOperand, Math.Pow(firstOperand, secondOperand));
+            }
         }
     }
 }
